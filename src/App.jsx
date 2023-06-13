10189e245ec59e790536a5227b9572eea2c39571
Pablo Silva <pmarcelosilvag@gmail.com>
2023-06-13 18:07:50 -0400

    refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import axios from 'axios'
 
@@ -8,51 +8,50 @@ import { WeatherCard } from './components/WeatherCard'
 function App() {
   const [coords, setCoords] = useState()
   const [weather, setWeather] = useState()
-  
-  // El primer useEffect obtiene las coordenadas del usuario mediante la API de geolocalizacion de HTML5
-  useEffect (()=>{
-    
-    const succes=pos=>{
 
-      const objectPosition={
-        lat:pos.coords.latitude,
-        lon:pos.coords.longitude
-         
+  // El primer useEffect obtiene las coordenadas del usuario mediante la API de geolocalizacion de HTML5
+  useEffect(() => {
+
+    const succes = pos => {
+
+      const objectPosition = {
+        lat: pos.coords.latitude,
+        lon: pos.coords.longitude
+
+      }
+
+      setCoords(objectPosition)
+
     }
 
-    setCoords(objectPosition)
- 
-  }
-     
     navigator.geolocation.getCurrentPosition(succes)  // recibe tres callbacks pero no es obligatorio
-  },[])
+  }, [])
 
   // Consumimos la API 
-  useEffect (()=>{
-    
-    if(coords)
-    {  
-      const apiKey='6653c3a5060326e806fe5aede05e7a6a'
-      const URL=`https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${apiKey}`
-      
+  useEffect(() => {
+
+    if (coords) {
+      const apiKey = '6653c3a5060326e806fe5aede05e7a6a'
+      const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${apiKey}`
+
       axios.get(URL)
-      .then(res=>setWeather(res.data))
-      .catch(error=>console.log(error))
-       
+        .then(res => setWeather(res.data))
+        .catch(error => console.log(error))
+
     }
-  
-  },[coords])
-                    
+
+  }, [coords])
+
   console.log(weather);
 
-   
+
   return (
-    <div  className="App">
-      
-       {
-         weather? <WeatherCard weather={weather}/>:<Loading/> 
-       }
-   </div>
+    <div className="App">
+
+      {
+        weather ? <WeatherCard weather={weather} /> : <Loading />
+      }
+    </div>
   )
 }
 
--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -6,18 +6,18 @@ import cloudy from '../images/cloudy.png'
 import clouds_weather from '../images/clouds_weather.png'
 
 
-export const WeatherCard = ({weather}) => {
+export const WeatherCard = ({ weather }) => {
 
 
   const [isDegree, setisDegree] = useState(true)
-  const degree=()=>setisDegree(!isDegree)
+  const degree = () => setisDegree(!isDegree)
 
-  function toCelsius(){
-    let Celsius=weather.main.temp-273.15
+  function toCelsius() {
+    let Celsius = weather.main.temp - 273.15
     return Celsius
   }
-  function toFahrenheit(){
-    let Fahrenheit=1.8*((weather.main.temp)-273.15)+32
+  function toFahrenheit() {
+    let Fahrenheit = 1.8 * ((weather.main.temp) - 273.15) + 32
     return Fahrenheit
   }
 
@@ -25,53 +25,53 @@ export const WeatherCard = ({weather}) => {
 
   return (
     <div className='weather-card'>
-          <h2>Weather App</h2>  
-          <div className='weather-location'>
-          <p>Tiempo actual:&nbsp; &nbsp;<span>Ciudad &nbsp;</span>{`${weather.name} ${weather.sys.country}`}</p>
-            
-          </div>
-          <div className='weather-info'>
-            <div className='weather-icon'>
-              <img src={ weather?`https://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`:''} alt="" />
-            </div>
-            
-            <div className='weather-info-temp'>
-                <div className='weather-temperature'>{isDegree? `${(toCelsius()).toFixed(1)} ºC`:` ${(toFahrenheit()).toFixed(1)} ºF`}
-                </div>
+      <h2>Weather App</h2>
+      <div className='weather-location'>
+        <p>Current Time:&nbsp; &nbsp;<span>City: &nbsp;</span>{`${weather.name} ${weather.sys.country}`}</p>
 
-                <div className='weather-sensation'>
-                <span>Humidity &nbsp;</span>{`${weather.main.humidity} %`}
-                </div>
-                <div className='weather-description'>
-                  Description:&nbsp;{weather.weather[0].description}
-                </div>
-            </div>
-            <div className='button-degree'>
-                <button onClick={degree} className='weather-button'>{isDegree?'Change to ºF':'Change to ºC'}</button>
-            </div>
-           </div>
+      </div>
+      <div className='weather-info'>
+        <div className='weather-icon'>
+          <img src={weather ? `https://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png` : ''} alt="" />
+        </div>
 
-          <div className='weather-principal-info'>
-            
-            <div className='weather-speed'>
-            <img className='img_info' src={Wind} alt="" />
-            Wind speed <span>{`${weather.wind.speed} m/s`}</span>
-            </div >
-            <div className='weather-clouds'>
-            <img className='img_info' src={cloud} alt="" />
-            Clouds     <span>{`${weather.clouds.all} %`}</span> 
-            </div>
-            <div className='weather-pressure' >
-            <img className='img_info' src={cloudy} alt="" />
-            Pressure   <span>{`${weather.main.pressure} mb`}</span>
-            </div>
-           
-            <div className='weather-vis' >
-            <img className='img_info' src={clouds_weather} alt="" />
-            Visibility <span>{`${weather.visibility/1000} Km`}</span>  
-            </div>
+        <div className='weather-info-temp'>
+          <div className='weather-temperature'>{isDegree ? `${(toCelsius()).toFixed(1)} ºC` : ` ${(toFahrenheit()).toFixed(1)} ºF`}
           </div>
-        
+
+          <div className='weather-sensation'>
+            <span>Humidity &nbsp;</span>{`${weather.main.humidity} %`}
+          </div>
+          <div className='weather-description'>
+            Description:&nbsp;{weather.weather[0].description}
+          </div>
+        </div>
+        <div className='button-degree'>
+          <button onClick={degree} className='weather-button'>{isDegree ? 'Change to ºF' : 'Change to ºC'}</button>
+        </div>
+      </div>
+
+      <div className='weather-principal-info'>
+
+        <div className='weather-speed'>
+          <img className='img_info' src={Wind} alt="" />
+          Wind speed <span>{`${weather.wind.speed} m/s`}</span>
+        </div >
+        <div className='weather-clouds'>
+          <img className='img_info' src={cloud} alt="" />
+          Clouds     <span>{`${weather.clouds.all} %`}</span>
+        </div>
+        <div className='weather-pressure' >
+          <img className='img_info' src={cloudy} alt="" />
+          Pressure   <span>{`${weather.main.pressure} mb`}</span>
+        </div>
+
+        <div className='weather-vis' >
+          <img className='img_info' src={clouds_weather} alt="" />
+          Visibility <span>{`${weather.visibility / 1000} Km`}</span>
+        </div>
+      </div>
+
     </div>
 
   )
